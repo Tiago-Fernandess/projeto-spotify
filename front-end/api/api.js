34e591798f9d835847cc6f7f886137260d74bc19
Tiago Fernandes <tiago212005@gmail.com>
2025-03-19 10:35:12 -0300

    Ajustes para rodar API

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,7 +1,7 @@
 // Fetch ou Axios
 import axios from "axios";
 
-const URL = "https://projeto-spotify-li5x.onrender.com/3000";
+const URL = "https://projeto-spotify-li5x.onrender.com";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
